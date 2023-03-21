e7db67929d1c79368c96057d687ae444334b2278
MoJaeYoung <jym2013@gmail.com>
2023-03-21 19:09:01 +0900

    0321 수업 끝

--- rlandboot3/src/main/java/kr/co/rland/web/entity/Category.java
+++ rlandboot3/src/main/java/kr/co/rland/web/entity/Category.java
@@ -14,42 +14,44 @@ import lombok.ToString;
 //@AllArgsConstructor
 //@ToString
 
-@Data
-@Builder
+//@Data
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor //
 public class Category {
-//	public Category(int id, String name) {
-//		super();
-//		this.id = id;
-//		this.name = name;
-//	}
+	public Category(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
 
 	private int id;
 	private String name;
 	
-//	public Category() {
-//		// TODO Auto-generated constructor stub
-//	}
-//
-//	public int getId() {
-//		return id;
-//	}
-//
-//	public void setId(int id) {
-//		this.id = id;
-//	}
-//
-//	public String getName() {
-//		return name;
-//	}
-//
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return "Category [id=" + id + ", name=" + name + "]";
-//	}
+	public Category() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", name=" + name + "]";
+	}
 	
 	
 	
